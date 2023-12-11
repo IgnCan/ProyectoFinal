d0e7d5d3d54f71a38f839e6e1148b2bc08db920b
IgnCan <igcandia2017@udec.cl>
2023-12-11 14:09:30 -0300

    MejorasPrototipoSistCompra

--- Pruebas/MainFrame.java
+++ Pruebas/MainFrame.java
@@ -3,6 +3,92 @@ package Pruebas;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+//public class MainFrame extends JFrame {
+//
+//    private ArrayList<ArrayList<Object>> listaPrincipal;
+//
+//    public MainFrame() {
+//        listaPrincipal = new ArrayList<>();
+//
+//        // Agregar sublistas predefinidas con JButton
+//        JButton boton1 = new JButton("Botón 1");
+//        JButton boton2 = new JButton("Botón 2");
+//
+//        agregarSubListaPredefinida("identificador1", boton1, boton2);
+//        agregarSubListaPredefinida("identificador2", boton1, boton2);
+//        agregarSubListaPredefinida("identificador3", boton1, boton2);
+//
+//        // Configurar el JFrame
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        setTitle("Agregar Sub-Lista");
+//        setSize(300, 200);
+//
+//        // Crear un JPanel con el botón y agregarlo al JFrame
+//        String nuevoIdentificador = "hola";
+//        SubListPanel subListPanel = new SubListPanel(this::agregarNuevaSubLista, listaPrincipal, nuevoIdentificador);
+//        add(subListPanel);
+//
+//        // Mostrar el JFrame
+//        setVisible(true);
+//    }
+//
+//    private void agregarNuevaSubLista(String nuevoIdentificador) {
+//        if (!existeIdentificador(nuevoIdentificador)) {
+//            // Si el identificador no existe, crear una nueva sub-lista
+//            ArrayList<Object> nuevaSubLista = new ArrayList<>();
+//            nuevaSubLista.add(nuevoIdentificador);
+//
+//            // Agregar dos JButton como elementos a la sublista
+//            nuevaSubLista.add(new JButton("Botón 1"));
+//            nuevaSubLista.add(new JButton("Botón 2"));
+//
+//            // Puedes agregar más elementos a la sublista si es necesario
+//            listaPrincipal.add(nuevaSubLista);
+//
+//            JOptionPane.showMessageDialog(this, "Nueva sub-lista creada con identificador " + nuevoIdentificador);
+//        } else {
+//            JOptionPane.showMessageDialog(this, "Ya existe una sub-lista con el identificador " + nuevoIdentificador);
+//        }
+//    }
+//
+//    private void agregarSubListaPredefinida(String identificador, JButton boton1, JButton boton2) {
+//        // Crear una sublista predefinida
+//        ArrayList<Object> subLista = new ArrayList<>();
+//        subLista.add(identificador);
+//
+//        // Agregar los botones a la sublista
+//        subLista.add(boton1);
+//        subLista.add(boton2);
+//
+//        // Puedes agregar más elementos a la sublista si es necesario
+//
+//        // Agregar la sublista predefinida a la lista principal
+//        listaPrincipal.add(subLista);
+//    }
+//
+//    private boolean existeIdentificador(String identificador) {
+//        for (ArrayList<Object> subLista : listaPrincipal) {
+//            String subListaIdentificador = (String) subLista.get(0); // Suponiendo que el identificador está en la posición 0
+//            if (subListaIdentificador.equals(identificador)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//
+//    public static void main(String[] args) {
+//        SwingUtilities.invokeLater(MainFrame::new);
+//    }
+//}
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+import javax.swing.*;
+import java.util.ArrayList;
 
 public class MainFrame extends JFrame {
 
@@ -25,30 +111,32 @@ public class MainFrame extends JFrame {
         setSize(300, 200);
 
         // Crear un JPanel con el botón y agregarlo al JFrame
-        SubListPanel subListPanel = new SubListPanel(this::agregarNuevaSubLista, listaPrincipal);
+        String nuevoIdentificador = "identificador1";
+        SubListPanel subListPanel = new SubListPanel(this::agregarNuevaSubLista, listaPrincipal, nuevoIdentificador);
         add(subListPanel);
 
         // Mostrar el JFrame
         setVisible(true);
     }
 
-    private void agregarNuevaSubLista() {
-        String nuevoIdentificador = "hola"; // Puedes cambiar esto por una entrada del usuario
+    private void agregarNuevaSubLista(String nuevoIdentificador) {
         if (!existeIdentificador(nuevoIdentificador)) {
             // Si el identificador no existe, crear una nueva sub-lista
             ArrayList<Object> nuevaSubLista = new ArrayList<>();
             nuevaSubLista.add(nuevoIdentificador);
 
             // Agregar dos JButton como elementos a la sublista
-            nuevaSubLista.add(new JButton("Botón 1"));
-            nuevaSubLista.add(new JButton("Botón 2"));
+            JButton boton1 = new JButton("Botón 1");
+            JButton boton2 = new JButton("Botón 2");
+            nuevaSubLista.add(boton1);
+            nuevaSubLista.add(boton2);
 
             // Puedes agregar más elementos a la sublista si es necesario
             listaPrincipal.add(nuevaSubLista);
 
             JOptionPane.showMessageDialog(this, "Nueva sub-lista creada con identificador " + nuevoIdentificador);
         } else {
-            JOptionPane.showMessageDialog(this, "Ya existe una sub-lista con el identificador " + nuevoIdentificador);
+            mostrarBotonesDeSubLista(nuevoIdentificador);
         }
     }
 
@@ -77,7 +165,22 @@ public class MainFrame extends JFrame {
         return false;
     }
 
+    private void mostrarBotonesDeSubLista(String identificador) {
+        for (ArrayList<Object> subLista : listaPrincipal) {
+            String subListaIdentificador = (String) subLista.get(0);
+            if (subListaIdentificador.equals(identificador)) {
+                // Mostrar los botones de la sublista en el JPanel
+                SubListPanel subListPanel = (SubListPanel) getContentPane().getComponent(0);
+                subListPanel.mostrarBotones(subLista);
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(MainFrame::new);
     }
 }
+
+
+
--- Pruebas/SubListPanel.java
+++ Pruebas/SubListPanel.java
@@ -6,30 +6,103 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+//public class SubListPanel extends JPanel {
+//
+//    private SubListCallback subListCallback;
+//    private ArrayList<ArrayList<Object>> listaPrincipal;
+//    private String nuevoIdentificador;
+//
+//    public SubListPanel(SubListCallback subListCallback, ArrayList<ArrayList<Object>> listaPrincipal, String nuevoIdentificador) {
+//        this.subListCallback = subListCallback;
+//        this.listaPrincipal = listaPrincipal;
+//        this.nuevoIdentificador = nuevoIdentificador;
+//
+//        setLayout(new FlowLayout());
+//
+//        // Crear botón para agregar nueva sub-lista
+//        JButton agregarSubListaButton = new JButton("Agregar Sub-Lista");
+//        agregarSubListaButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                subListCallback.mostrarSubLista(nuevoIdentificador);
+//            }
+//        });
+//        add(agregarSubListaButton);
+//    }
+//
+//    public interface SubListCallback {
+//        void mostrarSubLista(String nuevoIdentificador);
+//    }
+//}
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
 public class SubListPanel extends JPanel {
 
-    private MainFrame mainFrame;
     private SubListCallback subListCallback;
     private ArrayList<ArrayList<Object>> listaPrincipal;
+    private String nuevoIdentificador;
 
-    public SubListPanel(SubListCallback subListCallback, ArrayList<ArrayList<Object>> listaPrincipal) {
+    // Componentes para mostrar botones
+    private JButton boton1;
+    private JButton boton2;
+
+    public SubListPanel(SubListCallback subListCallback, ArrayList<ArrayList<Object>> listaPrincipal, String nuevoIdentificador) {
         this.subListCallback = subListCallback;
         this.listaPrincipal = listaPrincipal;
+        this.nuevoIdentificador = nuevoIdentificador;
 
         setLayout(new FlowLayout());
 
+        // Crear campo de texto para el identificador
+        JTextField identificadorTextField = new JTextField(10);
+        add(identificadorTextField);
+
         // Crear botón para agregar nueva sub-lista
         JButton agregarSubListaButton = new JButton("Agregar Sub-Lista");
-        agregarSubListaButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                subListCallback.mostrarSubLista();
-            }
-        });
+        agregarSubListaButton.addActionListener(e -> subListCallback.mostrarSubLista(nuevoIdentificador));
         add(agregarSubListaButton);
+
+        // Inicializar los botones (se muestran después)
+        boton1 = new JButton("Botón 1");
+        boton2 = new JButton("Botón 2");
     }
 
     public interface SubListCallback {
-        void mostrarSubLista();
+        void mostrarSubLista(String nuevoIdentificador);
+    }
+
+    // Método para mostrar los botones de la sublista
+    public void mostrarBotones(ArrayList<Object> subLista) {
+        removeAll(); // Limpiar el panel antes de agregar componentes
+        // Obtener identificador y botones de la sublista
+        String identificador = (String) subLista.get(0);
+        boton1 = (JButton) subLista.get(1);
+        boton2 = (JButton) subLista.get(2);
+
+        // Agregar el identificador y los botones al panel
+        add(new JLabel("Identificador: " + identificador));
+        add(boton1);
+        add(boton2);
+
+        // Volver a validar el layout
+        revalidate();
+        repaint();
     }
 }
+
+
+
+
